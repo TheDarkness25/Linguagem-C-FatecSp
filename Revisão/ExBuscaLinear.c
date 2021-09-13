c6d2047673f998aac14a2c23a10b11ce9adce44c
Rafael Venticinque <rafastela59@gmail.com>
2021-09-13 15:35:47 -0300

    Rev/Busca

---
+++ Revisão/ExBuscaLinear.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int PercorreVetor(int v[20], int n, int t){
+
+	for(i=1; i < t; i++){
+		if(v[1] == n){
+			return 1;
+			}
+		}
+	return 0;
+	}
+
+int main(void){
+	printf("Estrutura de dados!");
+	int v[] = {1, 2, 3, 4, 5, 6};
+	int t= 6;
+    printf("%d\n", PercorreVetor(v, 5, t));
+	return 0;
+	}
